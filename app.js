fe5a03a7371f8708fdd56f789aea3d726e0208db
Claudio Y <x_metalnos_x@hotmail.com>
2022-03-16 11:39:34 -0300

    Initial commit, 4.2 finished

---
+++ app.js
@@ -0,0 +1,29 @@
+const cors = require('cors');
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const blogsRouter = require('./controller/blogs');
+const config = require('./utils/config');
+const logger = require('./utils/logger');
+const middleware = require('./utils/middleware');
+
+const app = express();
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('Successfully connected to MongoDB');
+  })
+  .catch((error) => {
+    logger.error('Unable to connect to MongoDB:', error.message);
+  });
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+
+app.use('/api/blogs', blogsRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
---
+++ controller/blogs.js
@@ -0,0 +1,24 @@
+const { Router } = require('express');
+const Blog = require('../models/blog');
+
+const blogsRouter = Router();
+
+blogsRouter.get('/', (request, response) => {
+  Blog
+    .find({})
+    .then((blogs) => {
+      response.json(blogs);
+    });
+});
+
+blogsRouter.post('/', (request, response) => {
+  const blog = new Blog(request.body);
+
+  blog
+    .save()
+    .then((result) => {
+      response.status(201).json(result);
+    });
+});
+
+module.exports = blogsRouter;
